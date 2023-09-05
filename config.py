49bb7d4ff7b3a97e12124bfffd394e9e6d613e6d
jsbsjv <123757167+jsbsjv@users.noreply.github.com>
2023-09-05 14:10:46 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,14 +7,14 @@ from pytgcalls import PyTgCalls
 if os.path.exists(".env"):
     load_dotenv(".env")
 
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-SESSION = os.getenv("SESSION")
-OWNER_NAME = os.getenv("OWNER_NAME")
-CHANNEL = os.getenv("CHANNEL")
+API_ID = int(os.getenv("27340824"))
+API_HASH = os.getenv("a17d5851512b2af6b0d97008281f2740")
+SESSION = os.getenv("1ApWapzMBu5F7jVD3h0InU01KEXQHFMrMEnA6oEJ0qdMzl9hVtXmfhjIDa4zaHYWkCVV3lhaTHXwHVRPYTHJYi2eG6qMJCRe_1i9ptKV0jWRiBV3za1C1-M7CqU11rIMC6k32Ev7Cmxe6bXmT65WiZCxTN_O6v_1IXuCNxZsZ_t35Rh67rPEwK_E_iTJ9uaQYZWC-xOfwhXs7PZnfT249M0CFvivC2OxOSAlBhSYbH2-LWOVGq-c2HJwlgkJuW-w5j80etgzSqZiw9TQRvMDJxJ0BhwulBiqzeY3nHI328vIewRV-Km71YOYlsONR0Tq8B_JFMe51d9glJnBohDDxHa20Glq-Yzo=")
+OWNER_NAME = os.getenv("@EC_60")
+CHANNEL = os.getenv("@WWEEHHHH")
 PHOTO_CH = os.getenv("PHOTO_CH")
 HNDLR = os.getenv("HNDLR", "")
-SUDO_USERS = list(map(int, os.getenv("SUDO_USERS").split()))
+SUDO_USERS = list(map(int, os.getenv("@SC_60").split()))
 
 
 contact_filter = filters.create(
